07dd7dc1ca0756b75097fe5f4d4f391f3f08485e
MrkdtsCodes <datusmarkandriedalisay@gmail.com>
2026-04-07 18:17:09 +0800

    Add construct function for dynamic passing of value, add Setters and getters, Tried inheriting AdminUser class to User class

--- OOP.php
+++ OOP.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class User
 {
 
@@ -8,30 +7,73 @@ class User
     // public $email = "datusmarkandrie@gmailcoms";
 
     public $name; //properties
-    public $email;
+    private $email;
     public $friendrequest;
-
-    public function __construct($name, $email, $friendrequest)
+    public function __construct($name, $email, $friendrequest) //constructor for dynamic
     {
         $this->name = $name;
         $this->email = $email;
         $this->friendrequest = $friendrequest;
     }
 
-    public function addfriend()
+    public function getEmail() //getters
     {
-        return "$this->name sent an friend request to $this->friendrequest";
+        return $this->email;
+    }
+
+    public function setemail($email) //setters updating 45 to valid email
+    {
+        $emailregex = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+        if (!preg_match($emailregex, $email)) {
+            echo "not an Email";
+        } else {
+
+            echo $this->email = $email . '<br>';
+        }
+    }
+
+    public function addfriend() //methods just like a functions
+    {
+        return "$this->email sent an friend request to $this->friendrequest";
     } //methods 
+
+}
+
+class AdminUser extends User{ //inheriting user properties
+
+        public $adminrole; //acces modifiers
+
+        public function __construct($name, $email, $friendrequest, $adminrole)
+        {
+            $this->adminrole = $adminrole;
+            parent::__construct($name, $email, $friendrequest);
+            
+        }
+
+        public function getrole(){
+           echo "role: $this->adminrole <br>";
+
+        }
+
 }
 
 
 
-//instanciation
+//instanciation creating instanciation
 
-$newUserOne = new User('Mark Andrie Datus', 'datusmarkandrie@gmail.com', 'Elmira Labarnes');
+$newUserOne = new User('Mark Andrie Datus', '43', 'Elmira Labarnes');
+$newAdminUser = new AdminUser('Linux Torvalds', 'linux@thegnu@linux.com', 'Mark Andrie Datus', 'Administrator');
 
+//invoking calling and pasing data to a class 
 
-echo $newUserOne->addfriend();
+echo $newUserOne->addfriend() . '<br>';
+echo $newUserOne->setemail('datusmarkandrie@gmail.com');
+echo $newAdminUser->name ;
+echo '<br>';
+echo $newAdminUser->getEmail();
+echo '<br>';
+echo $newAdminUser->getrole();
+
 //access a value in a class changing manually 
 // echo $newUserOne->name . '<br>';
 // echo $newUserOne->email . '<br>';
